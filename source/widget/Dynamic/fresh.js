8510a67bb21a385352674e7d88d180b4472d0f93
duxinlixiaoxue <1207931454@qq.com>
2015-11-18 20:26:54 +0800

    Signed-off-by: duxinlixiaoxue <1207931454@qq.com>

--- source/widget/Dynamic/fresh.js
+++ source/widget/Dynamic/fresh.js
@@ -10,7 +10,7 @@
 var fresh = fresh || {};
 fresh.path = fresh.path || {};
 fresh.path.url = '/data/Dynamic/';
-fresh.path.img = '/static/pic/Dynamic/';
+fresh.path.img = '/static/img/Dynamic/';
 fresh.emoteHmtl = null;
 
 /**
