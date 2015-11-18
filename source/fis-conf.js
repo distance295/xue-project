e443040f26a6ae13f4cc11ed6d78ab84a9f60ae3
Marco.Pai <mr.pai@msn.com>
2015-11-18 19:09:39 +0800

    整理配置文件，增加注释

--- source/fis-conf.js
+++ source/fis-conf.js
@@ -1,6 +1,7 @@
 //fis.hook('module', {
 //    mode: 'amd' // 模块化支持 amd 规范，适应 require.js
 //});
+
 fis.set('project.ignore', [
     'output/**',
     'node_modules/**',
@@ -9,6 +10,7 @@ fis.set('project.ignore', [
     '/static/**',
     'widget/**/*.html',
     'widget/**/*.css',
+    'widget/**/*.md',
     '/config/**',
     '/components/**',
     '/lib/foundation/**',
@@ -42,67 +44,59 @@ fis.match('::packager', {
 //        }
 //    }
 //});
-fis.match('*.less', {
-    parser: fis.plugin('less'), //启用fis-parser-less插件
-    rExt: '.css'
-})
-//fis.match('/**/*.tpl', {
-//    isHtmlLike: true,
-//    release: false
-//})
-
-//fis.match('::image', {
-//    release : '/static/img/$0'
-////    packTo: '/static/img/$0',
-////    url: '/static/img/$0'
-//})
-fis.match('/widget/**/*.html', {
-    packTo: '/temp/html.temp'
-//    release:false
-})
 
 
 
 fis.media('tmpl')
+
+// 主模板文件规则
     .match('/template/(**)/(*).tpl', {
         isHtmlLike: true,
         rExt: '.html',
         release: '/pages/$1.$2.html'
     })
-    .match('/widget/**/*.tpl', {
-        isHtmlLike: true,
-        packTo: '/temp/temp.html',
+
+// 禁止发布的文件
+    .match('/widget/(**)/*.{md,tpl,html}', {
         release: false
     })
 
+// 不做修改直接拷贝的目录
     .match('/lib/*', {
         release: '/lib/$0'
     })
     .match('/data/*', {
         release: '/data/$0'
     })
+
+// JSON文件规则
     .match('/widget/(**)/(*.json)', {
         release: '/data/$1/js/$2',
     })
-    .match('/widget/(**)/*.md', {
-        release: false
-    })
+
+// JS规则
     .match('/widget/(**)/*.js', {
         release: '/static/js/$1.js',
     })
+
+// Less规则
     .match('/widget/(**)/*.less', {
         parser: fis.plugin('less'), //启用fis-parser-less插件
         rExt: '.css',
         release: '/static/css/$1.css'
     })
-    // 这里是资源图片规则
+
+// pic资源图片规则
     .match('/widget/(*)/pic/(*.{png,jpg,gif,cur})', {
         release: '/static/pic/$1$3$5$7/$2$4$6$8'
     })
-    // 这里是素材图片规则
+
+// img素材图片规则
     .match('/widget/(*)/img/(*.{png,jpg,gif,cur})', {
         release: '/static/img/$1$3$5$7/$2$4$6$8'
     })
+
+// img里面存在文件夹时的规则
     .match('/widget/(**)/img/(**)/(*.png)', {
         release:'/static/img/$1/$2/$3'
     })
