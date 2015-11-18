42c76fecae923789775d75ba772d4d4b089b9f9a
Yvonne92 <yyloveyou0815@hotmail.com>
2015-11-18 20:30:08 +0800

    模态框fix
    模态框fix

--- source/template/UserHome/GoldIndex.tpl
+++ source/template/UserHome/GoldIndex.tpl
@@ -26,7 +26,5 @@
     };
 </script>
 
-
-
 <!-- 公共底部 -->
 <link rel="import" href="../Layer/layer.UserHome.foot.tpl?__inline">
--- source/widget/Module.Modal/Modal.js
+++ source/widget/Module.Modal/Modal.js
@@ -1,50 +1,19 @@
 /**
  * Created by yangmengyuan on 15/11/17.
  */
-//var createModal = createModal || {},
-//    $uiCreateModal = $('.ui-createModal');
-//
-//createModal.opt = {};
-//createModal.target = '';
-//
-//createModal.opt = {
-//    title :'modalTitle',
-//    cls :'modalClass',
-//    content : 'modalBody'
-//};
-//createModal.show = function(e){
-//    $('body').append("<div class='modal fade "+createModal.opt.cls+"'tabindex='-1' role='dialog'><div class='modal-dialog' role='document'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button><h4 class='modal-title'>"+createModal.opt.title+"</h4></div><div class='modal-body'>"+createModal.opt.content+"</div></div>")
-//    $('.modal').on('hidden.bs.modal',function(e){
-//        $(this).remove();
-//    })
-//};
-//createModal.init = function(o){
-//    $.extend(this.opt, o);
-//    $.each(arguments,function(k,v){
-//        createModal.opt[v.cls] = v;
-//    })
-//    $uiCreateModal.on('click',function(){
-//        //console.log(this);
-//        createModal.show(this);
-//    })
-//};
 var createModal = createModal || {},
     $uiCreateModal = $('.ui-createModal');
 
 createModal.opt = {};
 createModal.target = '';
 createModal.show = function(e){
-    $('body').append("<div class='modal fade "+createModal.opt[createModal.target].cls+"'tabindex='-1' role='dialog'><div class='modal-dialog' role='document'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button><h4 class='modal-title'>"+createModal.opt[createModal.target].title+"</h4></div><div class='modal-body'>"+createModal.opt[createModal.target].content+"</div></div>")
+    $.extend(this.opt, e);
+    console.log(this.opt);
+    $('body').append("<div id='"+ this.opt.id +"' class='modal fade "+this.opt.cls+"' tabindex='-1' role='dialog'><div class='modal-dialog' role='document'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button><h4 class='modal-title'>"+this.opt.title+"</h4></div><div class='modal-body'>"+this.opt.content+"</div></div>");
     $('.modal').on('hidden.bs.modal',function(e){
         $(this).remove();
     })
 };
-createModal.init = function(o){
-    $.each(arguments,function(k,v){
-        createModal.opt[v.cls] = v;
-    });
-    $uiCreateModal.on('click',function(){
-        createModal.target = $(this).attr('data-target').replace('.','');
-        createModal.show();
-    })
-};
+
+
+
