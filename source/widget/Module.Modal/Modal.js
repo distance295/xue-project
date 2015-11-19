f085e40501f896096a287e5bfedbe19ff4042adb
Yvonne92 <yyloveyou0815@hotmail.com>
2015-11-19 15:30:05 +0800

    模态框增加宽度
    模态框增加宽度

--- source/widget/Module.Modal/Modal.js
+++ source/widget/Module.Modal/Modal.js
@@ -1,15 +1,14 @@
 /**
  * Created by yangmengyuan on 15/11/17.
  */
-var createModal = createModal || {},
-    $uiCreateModal = $('.ui-createModal');
+var createModal = createModal || {};
 
 createModal.opt = {};
 createModal.target = '';
 createModal.show = function(e){
     $.extend(this.opt, e);
-    console.log(this.opt);
-    $('body').append("<div id='"+ this.opt.id +"' class='modal fade "+this.opt.cls+"' tabindex='-1' role='dialog'><div class='modal-dialog' role='document'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button><h4 class='modal-title'>"+this.opt.title+"</h4></div><div class='modal-body'>"+this.opt.content+"</div></div>");
+    //console.log(this.opt);
+    $('body').append("<div id='"+ this.opt.id +"' class='modal fade "+this.opt.cls+"' tabindex='-1' role='dialog'><div class='modal-dialog' role='document' style='width: "+this.opt.width+"px'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button><h4 class='modal-title'>"+this.opt.title+"</h4></div><div class='modal-body'>"+this.opt.content+"</div></div>");
     $('.modal').on('hidden.bs.modal',function(e){
         $(this).remove();
     })
