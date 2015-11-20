013e56a8a574885781908a56f00c7d4c26663a22
Marco.Pai <mr.pai@msn.com>
2015-11-20 19:12:34 +0800

    更新bootstrap框架

--- source/lib/bootstrap/3.3.5-custom/js/bootstrap.js
+++ source/lib/bootstrap/3.3.5-custom/js/bootstrap.js
@@ -5,8 +5,8 @@
  */
 
 /*!
- * Generated using the Bootstrap Customizer (http://getbootstrap.com/customize/?id=203bf1f7cf7b440fb91c)
- * Config saved to config.json and https://gist.github.com/203bf1f7cf7b440fb91c
+ * Generated using the Bootstrap Customizer (http://getbootstrap.com/customize/?id=dc368bbdd020ab9a292c)
+ * Config saved to config.json and https://gist.github.com/dc368bbdd020ab9a292c
  */
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
