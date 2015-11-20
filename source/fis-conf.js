848e872fab2074191b18160687c6770f493529d4
Marco.Pai <mr.pai@msn.com>
2015-11-20 19:35:25 +0800

    配置文件

--- source/fis-conf.js
+++ source/fis-conf.js
@@ -7,7 +7,8 @@ fis.set('project.ignore', [
     'widget/**/*.css',
     'widget/**/*.md',
     '/config/**',
-    // '/static/**',
+//     '/static/**',
+    '/static/**/*.less',
     '/components/**',
     '/lib/foundation/**',
     '/lib/Font-Awesome/master/**',
@@ -15,6 +16,7 @@ fis.set('project.ignore', [
     'fis-conf-pages.js',
     '/pages/**',
     '/template/Layer/*',
+//    '/template/**/*.tpl',
     '/**/*.bat',
     '*.bat',
     '*.sh',
@@ -24,11 +26,15 @@ fis.set('project.ignore', [
 fis.match('::packager', {
     packager: fis.plugin('map'),
     postpackager: fis.plugin('loader', {
-//        allInOne: true
+
     })
 });
 
 
+// 不做修改直接拷贝的目录
+fis.match('/lib/*', {
+    release: '/lib/$0'
+})
 
 fis.media('tmpl')
 
@@ -38,15 +44,9 @@ fis.media('tmpl')
         rExt: '.html',
         release: '/pages/$1/$2.html'
     })
-
 // 禁止发布的文件
     .match('/widget/(**)/*.{md,tpl,html}', {
         release: false
-    })
-
-// 不做修改直接拷贝的目录
-    .match('/lib/*', {
-        release: '/lib/$0'
     })
     .match('/data/*', {
         release: '/data/$0'
@@ -83,12 +83,76 @@ fis.media('tmpl')
     .match('/widget/(**)/img/(**)/(*.png)', {
         release:'/static/img/$1/$2/$3'
     })
-
-
-
-
-    
-    
-    
-
 ;
+
+
+
+
+
+
+
+fis.media('home')
+    .match('::packager', {
+        packager: fis.plugin('map'),
+        postpackager: fis.plugin('loader', {
+            processor : {
+                '.less':'css',
+                '.tpl':'html'
+            },
+            allInOne: true,
+            css:'/static/css/aio/${filepath}_aio.css'
+        })
+    })
+// 主模板文件规则
+    .match('/template/UserHome/(*).tpl', {
+        isHtmlLike: true,
+        rExt: '.html',
+        release: '/pages/$1.html'
+    })
+    .match('/template/{UserManage,Mall,Layer}/*', {
+        release: false
+    })
+// 禁止发布的文件
+    .match('/widget/(**)/*.{md,tpl,html}', {
+        release: false
+    })
+    .match('/data/*', {
+        release: '/data/$0'
+    })
+
+// JSON文件规则
+    .match('/widget/(**)/(*.json)', {
+        release: '/data/$1/$2',
+    })
+
+// JS规则
+    .match('/widget/(**)/(*.js)', {
+//        packTo: '/static/js/aio/$2.js',
+//        release:false
+//        release: '/static/js/$1.$2'
+    })
+
+// Less规则
+    .match('/widget/(**)/(*.less)', {
+        parser: fis.plugin('less'), //启用fis-parser-less插件
+        rExt: '.css',
+//        packTo: '/static/css/aio/$1.$2.css'
+//        release: '/static/css/$1/$2'
+    })
+
+// pic资源图片规则
+    .match('/widget/(*)/pic/(*.{png,jpg,gif,cur})', {
+        release: '/static/pic/$1$3$5$7/$2$4$6$8'
+    })
+
+// img素材图片规则
+    .match('/widget/(*)/img/(*.{png,jpg,gif,cur})', {
+        release: '/static/img/$1$3$5$7/$2$4$6$8'
+    })
+
+// img里面存在文件夹时的规则
+    .match('/widget/(**)/img/(**)/(*.png)', {
+        release:'/static/img/$1/$2/$3'
+    })
+;
+
