c00fcbc2b5d12911d05b672fa01d8123010048f5
Marco.Pai <mr.pai@msn.com>
2015-11-27 22:54:48 +0800

    增加用户设置本地发布规则

--- source/fis-conf.js
+++ source/fis-conf.js
@@ -15,11 +15,23 @@ fis.set('project.ignore', [
     'fis-conf-pages.js',
     '/pages/**',
     '/template/Layer/*',
-    '/**/*.bat',
     '*.bat',
     '*.sh',
     '*.log',
-    '**/*.map'
+    '**/*.map',
+    'widget/courses/**',
+    'widget/courseStudycenter/**',
+    'widget/errorQuestion/**',
+    'widget/filter/**',
+    'widget/find-teacher/**',
+    'widget/footer/**',
+    'widget/fresh/**',
+    'widget/glory/**',
+    'widget/gold/**',
+    'widget/header/**',
+    'widget/live/**',
+    'widget/notice/**',
+    'widget/_homework/**'
 ]);
 fis.match('::packager', {
     packager: fis.plugin('map'),
@@ -158,5 +170,127 @@ fis.media('home')
     })
 ;
 
+// ==============================
 
+var __ignore = {
+    account : [
+        'homework',
+        'help',
+        'courseInfor',
+        'indexTrends',
+        'Mall.FindTeacher',
+        'Mall.live',
+        'Mall.personCenter',
+        'myCollect',
+        'perfect-information',
+        'personalCenter',
+        'Public.Dynamic',
+        'Public.FocusPic',
+        'Public.Footer',
+        'Public.Header',
+        'Public.MiniCart',
+        'Public.Module',
+        'Public.Nav',
+        'studyCourse',
+        'user.manage',
+        'UserHome.alert',
+        'UserHome.courses',
+        'UserHome.gold',
+        'UserHome.notice',
+        'UserHome.sidebar',
+        'UserHome.wrongQues'
+    ].toString()
+};
+
+fis.media('um')
+    .match('::packager', {
+        packager: fis.plugin('map'),
+        postpackager: fis.plugin('loader', {
+            allInOne: true,
+            processor : {
+                '.less':'css',
+                '.tpl':'html'
+            }
+        })
+    })
+    // 主模板文件规则
+    .match('/template/UserManage/(*).tpl', {
+        isHtmlLike: true,
+        rExt: '.html',
+        release: '/pages/$1.html'
+    })
+    .match('/template/{UserHome,Mall,Layer}/*', {
+        release: false
+    })
+
+    // 禁止发布的文件
+    .match('/widget/(**)/*.{md,tpl,html,css,mp3}', {
+        release: false
+    })
+    .match('/static/less/(*)',{
+        parser: fis.plugin('less'),
+        rExt: '.css',
+        release: '/static/css/$1',
+    })
+    .match('/data/**', {
+        release: false
+    })
+
+    .match('/lib/*', {
+        release: '/lib/$0',
+        packTo: '/lib/$0'
+    })
+    
+    .match('/data/Public.Nav/nav.json',{
+        release: '/data/Public.Nav.json'
+    })
+    // JSON文件规则
+    .match('/widget/(**)/(*.json)', {
+        release: false
+    })
+    // JS规则
+    .match('/widget/(**)/(*.js)', {
+        release: '/static/js/_temp.js',
+        packTo: '/static/js/xue.usermanage.js'
+    })
+    // Less规则
+    .match('/widget/(**)/(*.less)', {
+        parser: fis.plugin('less'),
+        rExt: '.css',
+        release: '/static/css/_temp.css',
+        packTo: '/static/css/xue.usermanage.css'
+    })
+
+    // pic资源图片规则
+    .match('/widget/(*)/pic/(*.{png,jpg,gif,cur})', {
+        release: '/static/pic/$2$4$6$8'
+    })
+    // img素材图片规则
+    .match('/widget/(*)/img/(*.{png,jpg,gif,cur})', {
+        release: '/static/img/$2$4$6$8'
+    })
+    // img里面存在文件夹时的规则
+    .match('/widget/(**)/img/(**)/(*.png)', {
+        release:'/static/img/$3'
+    })
+
+    // ----------------
+    .match('/widget/{'+ __ignore.account +'}/**', {
+        release: false
+    })
+    .match('/static/pic/Dynamic/*', {
+        release: false
+    })
+    .match('/widget/Public.Topbar/img/*', {
+        release: false
+    })
+    // ----------------
+
+    .match('/conf/fis-conf-account.js',{
+        release: '/fis-conf.js'
+    })
+    .match('/conf/deploy-account.bat',{
+        release: '/deploy.bat'
+    })
+;
 
