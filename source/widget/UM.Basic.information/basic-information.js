90d93af3124f0e713bd4627e85f4dadf0c139e78
yangjingjing <351299772@qq.com>
2015-12-03 19:19:02 +0800

    提交

--- source/widget/UM.Basic.information/basic-information.js
+++ source/widget/UM.Basic.information/basic-information.js
@@ -75,6 +75,7 @@ $.fn.nicknameajax = function(){
             url : '/MyInfos/getNicknameUseful',
             type : 'GET',
             dataType : 'json',
+            data : 'nickname=' + $('.nickname').val(),
             timeout: 7000,
             async: false,
             success  : function(result){
@@ -124,25 +125,13 @@ $('.school').on('blur',function(){
 $(function() {
     $(".btn-submit").click(function() {
        $.fn.nickname();
-       $.ajax({
-        type:"GET",
-        url:"/MyInfos/editStuInfo",
-        dataType: "json",
-        data: 'nickname=' + $('.nickname').val() + '&date=' + $('.date').val() + '&school=' + $('.school').val(),
-        timeout: 7000,
-        success: function(result) {
-          /* 填写的信息验证不通过 */
-          if(result.sign == 1){
-            window.location.href= '/Reg/RegSuc';
-          }else{
-            fCheck.setTips('.message-error span',result.msg);
-            setTimeout("fCheck.clearTips('.message-error span')",18000);
-          }
-        },
-        error: function() {
-          alert('数据读取错误,请重试..');
-          return false;
-        }
-       });
     })
-}) 
+})
+
+var messageError = $(".message-error span").is(":empty");
+if (messageError == '0') {
+    $('.message-error').css({
+        display: 'block'
+    });
+    setTimeout("fCheck.clearTips('.message-error')",6000); 
+}
