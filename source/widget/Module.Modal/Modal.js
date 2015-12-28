9e9a6d4064bd8dc467585f93e94e90f433ab0acc
Ge-yuan-jun <daydaygyj@gmail.com>
2015-12-28 11:37:23 +0800

    Signed-off-by: Ge-yuan-jun <daydaygyj@gmail.com>

--- source/widget/Module.Modal/Modal.js
+++ source/widget/Module.Modal/Modal.js
@@ -8,7 +8,7 @@ createModal.target = '';
 createModal.show = function(e){
     $.extend(this.opt, e);
     //console.log(this.opt);
-    $('body').append("<div id='"+ this.opt.id +"' class='modal fade modal-align "+this.opt.cls+"' tabindex='-1' role='dialog' style='margin:0 "+ -(this.opt.width/2) +"px;'><div class='modal-dialog' style='display:table-cell;vertical-align:middle;' role='document' style='width: "+this.opt.width+"px;'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button><h4 class='modal-title'>"+this.opt.title+"</h4></div><div class='modal-body'>"+this.opt.content+"</div></div>");
+    $('body').append("<div id='"+ this.opt.id +"' class='modal fade "+this.opt.cls+"' tabindex='-1' role='dialog'><div class='modal-dialog' style='width:"+ this.opt.width +"px;' role='document'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button><h4 class='modal-title'>"+this.opt.title+"</h4></div><div class='modal-body'>"+this.opt.content+"</div></div>");
     $('.modal').on('hidden.bs.modal',function(e){
         $(this).remove();
     })
