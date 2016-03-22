02764bd7beb2feb695577d33bc31b8294436458d
Yvonne92 <yyloveyou0815@hotmail.com>
2016-03-22 15:35:36 +0800

    fix

--- source/widget/Mall.live/live.js
+++ source/widget/Mall.live/live.js
@@ -2,6 +2,7 @@
  * Created by yangmengyuan on 15/10/22.
  */
 $(function(){
+    var $body = $('body');
     var $liveScrollBtn = $('.live-scroll-btn-container li');
     $liveScrollBtn.on('click',function(e){
         var $target = $(e.target);
@@ -9,7 +10,7 @@ $(function(){
         $liveScrollBtn.removeClass('live-scroll-btn-on').eq(index).addClass('live-scroll-btn-on');
         $('.live-scroll-box-container').animate({top:(-1*291*index) + 'px'},300)
     });
-    $('body').on({
+    $body.on({
         mouseenter:function(){
             $(this).find('.live-course-title').stop().animate({"height":80},300);
             $(this).find('.live-course-show-title').stop().fadeOut(300);
@@ -39,41 +40,62 @@ $(function(){
 
     });
 
-    $('body').on('click', '.live-order', function () {
-        //var presentid = $(this).closest('.gold-store-present-card').attr('id');
-        //$.ajax({
-        //    url: '/GoldShop/realAwardDetail',
-        //    //url:'/data/gold/gold-present-modal.html',
-        //    type: 'post',
-        //    //type:'get',
-        //    dataType: 'html',
-        //    data: {
-        //        id: presentid
-        //    },
-        //    success: function (result) {
-        //        if (result.substr(0, 4) == 'http' || result.substr(0, 1) == '/') {
-        //            window.location.href = result;
-        //            return;
-        //        }
-        //        liveOrderModal.showModal(result);
-        //    }
-        //})
-        liveOrderModal.showModal();
-
+    $body.on('click', '.live-order', function () {
+        var liveOrderId = $(this).closest('.live-card').attr('id');
+        $.ajax({
+            url : '/Lecture/ajaxFollow/',
+            type : 'post',
+            dataType : 'json',
+            data : {
+                liveId: liveOrderId
+            },
+            success : function(msg,event){
+                if(msg.sign == 2){
+                    window.location.href = msg.url;
+                    return;
+                }
+                if(msg.sign == 0){
+                    alert('您已预约过此课程');
+                }
+                if(msg.sign == 1){
+                    $(this).find('.live-order').attr({'data-target':'#liveOrderSuccessModal'});
+                    liveOrderSuccessModal.showModal();
+                }
+                if(msg.sign == 3){
+                    $(this).find('.live-order').attr({'data-target':'#liveOrderFailModal'});
+                    liveOrderFailModal.showModal();
+                }
+            }
+        });
     });
 
-    var liveOrderModal = liveOrderModal || {};
+    var liveOrderSuccessModal = liveOrderSuccessModal || {};
 
-    liveOrderModal.showModal = function(con){
+    liveOrderSuccessModal.showModal = function(con){
         var that = $(this), data = that.data();
-        var con = "<img src='img/orderSuccess.png'>";
+        var con = "<img src='/static/img/orderSuccess.png'><span class='orderSuccessTip'>5秒钟后关闭</span>";
         //console.log(data);
         createModal.show({
-            id : 'liveOrderModal',
-            width : '530',
+            id : 'liveOrderSuccessModal',
+            width : '560',
             title : "预约直播",
-            cls : "liveOrderModal aaa ccc",
+            cls : "liveOrderSuccessModal aaa ccc",
             content : con
         });
     };
+
+    var liveOrderFailModal = liveOrderFailModal || {};
+
+    liveOrderFailModal.showModal = function(con){
+        var that = $(this), data = that.data();
+        var con = "<img src='/static/img/orderFail.png'><span class='orderFailTip'>5秒钟后关闭</span>";
+        //console.log(data);
+        createModal.show({
+            id : 'liveOrderFailModal',
+            width : '560',
+            title : "预约直播",
+            cls : "liveOrderFailModal aaa ccc",
+            content : con
+        });
+    }
 });
