8796cde77d4e56a32552a40ca71df6601c37a296
xingzhenli <18701352612@163.com>
2016-12-27 14:54:24 +0800

    学习中心---我的发票页

--- source/widget/UM.bill/bill.js
+++ source/widget/UM.bill/bill.js
@@ -2,107 +2,11 @@
  * Created by yangmengyuan on 16/4/5.
  */
 //$(function(){
-    var $remarkFocus = $("#remarkFocus"),
-        $body = $('body'),
-        $blli = $(".bill-list li");
-
-    $blli.on('click', function(){
-        var that = $(this);
-        page = that.data('pages');
-        that.addClass("bill-tab").siblings().removeClass("bill-tab");
-        $(".bill-hidden").hide().eq($(this).index()).show();
-        var url = that.attr('data-url');
-        if(url == '/MyOrders/ajaxInvoiceOrder'){
-            billList(url,page);
-            noneAddress()
-        }else{
-            billTab();
-        }
-    });
-    $('.bill-list li:first').click();
-
-    var $bsb = $('.bill-sum-button');
-
-    function billList(url,page){
-
-        $.ajax({
-            type: "get",
-            url: url,
-            dataType: "html",
-            data:'curpage=' + page,
-            success: function(list){
-                var list = $.trim(list);
-                if(list.substr(0,1)=='<'){
-                    var box = $('.bill-details-list');
-                    box.html(list);
-                }else{
-                    if(list.substr(0,4)=='http' || list.substr(0,1)=='/'){
-                        window.location.href = list;
-                        return false;
-                    }
-                }
-                checkBox();
-            },
-            error:function(){
-                alert("异步失败");
-            }
-        });
-    };
-
-    function checkBox(){
-        $("input[type='checkbox']").click(function() {
-            var num = $body.find("input[type='checkbox']:checked").length;
-            $('.bill-sum-num em').html(num);
-            var sum = 0;
-            $body.find("input[type='checkbox']:checked").parents('.bill-details').find('.bill-li em').each(function(k,v){
-                sum += ($(v).text().match(/\d+/g)[0] * 1);
-            });
-            $('.bill-sum-price em').html(sum)
-            if($("input[type='checkbox']:checked").length == 0){
-                $bsb.css({'background-color':'#a0a0a0'});
-            }else{
-                $bsb.css({'background-color':'#3bafda'});
-            }
-        });
-    }
-
-    function noneAddress(){
-        var addr = $('.shipadd_list').text();
-        if(addr == ''){
-            $('.addr_switch span').hide();
-            $('.info_from').show();
-        }
-    }
-
-    function billTab(){
-        $.ajax({
-            type: "get",
-            url: "ajaxInvoiceApplyList",
-            dataType: "html",
-            data:{},
-            success: function(list){
-                var list = $.trim(list);
-                if(list.substr(0,1)=='<'){
-                    var box = $('.bill-apply'),
-                        content = $('.bill-content');
-                    box.html(list).show();
-                    content.css({'display':'none'});
-                }else{
-                    if(list.substr(0,4)=='http' || list.substr(0,1)=='/'){
-                        window.location.href = list;
-                        return false;
-                    }
-                }
-            },
-            error:function(){
-                alert("异步失败");
-            }
-        });
-    }
+    var $body = $('body');
 
     $body.on('click', '.bill-check-more', function() {
         var curpage = $('#page').val();
-        var url = "/MyOrders/ajaxInvoiceOrder/";
+        var url = "/MyOrders/ajaxStuInvoiceCouList";
         $('.load_container').remove();
         var loading ='<div class="loading_div"><i class="fa fa-spinner fa-spin fa-4"></i><span>加载中</span></div>';
         $(loading).appendTo('.bill-details-list');
@@ -118,7 +22,6 @@
                 if(resDat){
                     $('.loading_div').remove();
                     $(resDat).appendTo('.bill-details-list');
-                    checkBox()
                 }
                 else{
                     $('.loading_div').remove();
@@ -129,67 +32,10 @@
         });
     });
 
-    $remarkFocus
-        .focus(function(){
-            if($(this).val() == '选填,请填写备注信息'){
-                $(this).val("")
-            }
-        })
-        .blur(function(){
-            if($(this).val() == ''){
-                $(this).val("选填,请填写备注信息")
-            }
-        });
-
-    $body.on('click','.bill-apply-check',function(){
-        $('.bill-hidden').css({'display':'none'});
-        $('.bill-apply-check-detail').css({'display':'block'});
-    });
-
-    //关闭收货地址
-    $(".close_address").on('click',function(){
-        var that = $(this);
-        if($('.shipadd_list li').length > 0){
-            that.parents('#details_form').hide();
-        }
-    });
-    //展开收起更多收货地址
-    $('.addr_switch').on('click', function(event) {
-        var that = $(this);
-        if(that.hasClass('switch_off') !== true){
-            that.addClass('switch_off').find('span').text('收起地址');
-            that.prev().find('.f_detailAddress').show();
-        }else{
-            that.removeClass('switch_off').find('span').text('更多地址');
-            that.prev().find('.f_detailAddress').hide();
-        }
-
-    });
-
-    var $usi = $('.user_site_info');
-
-
-    //鼠标进入时增加样式
-    $usi.on('mouseenter', '.shipadd_list li', function(event) {
-        var that = $(this);
-        that.addClass('current');
-    });
-    //鼠标离开时删除样式
-    $usi.on('mouseleave', '.shipadd_list li', function(event) {
-        var that = $(this);
-        that.removeClass('current');
-    });
-    //选择收货人地址
-    $usi.on('click', '.consignee_item', function(event) {
-        var that = $(this);
-        that.addClass('current')
-            .parent().siblings()
-            .find('.consignee_item')
-            .removeClass('current');
-        //var _id = that.parents('li').attr('id');
-        // console.log(_id);
-        //$('#AddressId').val(_id);
-    });
+    // $body.on('click','.bill-apply-check',function(){
+    //     $('.bill-hidden').css({'display':'none'});
+    //     $('.bill-apply-check-detail').css({'display':'block'});
+    // });
 
     var courses = courses || {};
 
@@ -301,61 +147,154 @@
             courses.avatar.toggle(that)
         }
     });
-
-
-    //提交按钮
-    $bsb.on('click',function(event){
-        var checkInput = $body.find("input[type='checkbox']:checked");
-        var addId = $('input[type="radio"]:checked').attr('id').match(/\d+$/)[0],
-            billType = $('#bill-title-select').val(),
-            billTitle = $('.bill-title-input').val(),
-            billTip = $('#remarkFocus').val();
-        var arr = [],
-            textNum;
-        checkInput.parents('.bill-details').find('.bill-order-num em').each(function (k,v) {
-            textNum = $(this).text();
-            arr.push(textNum)
+    // 申请发票
+    $body.off('click', '.applyBox a').on('click', '.applyBox a', function(){
+        var maskPop = '<div class="bill-mask">'
+                        + '<div class="bill-pop">'
+                            + '<p class="billPop-title">完善发票信息'
+                                + '<a href="javascript:;" class="btn-close"><img src="http://res11.xesimg.com/account/img/close.png"></a>'
+                            + '</p>'
+                            + '<div class="invoice-type">'
+                                + '<span>请选择发票类型</span>'
+                                + '<select name="bill-title-select" id="bill-title-select">'
+                                    + '<option value="0">请选择</option>'
+                                    + '<option value="1">培训费</option>'
+                                    + '<option value="2">资料费</option>'
+                                + '</select>'
+                            + '</div>'
+                            + '<div class="invoice-title">'
+                                + '<span>发票抬头</span>'
+                                + '<input type="text" class="bill-title-input" placeholder="详细填写发票公司名称">'
+                            + '</div>'
+                            + '<p class="bill-tips">注：网校现仅支持电子发票，发票开具后请自行下载打印！</p>'
+                            + '<p class="bill-tips-error"></p>'
+                            + '<div class="invoice-btn">'
+                                + '<input type="button" class="btn-confirm" value="确认">'
+                                + '<input type="button" class="btn-cancel" value="取消">'
+                            + '</div>'
+                        + '</div>'
+                    +'</div>';
+        $(maskPop).appendTo('.some-bill');
+        var that = $(this);
+        var id = that.parents('.applyBill').attr("id");
+        $body.on('click', '.btn-close, .btn-cancel', function(){
+            $('.bill-mask').remove();
         });
-        if(checkInput.length == 0){
-            event.preventDefault();
-        }else {
-            if (billType == '请选择') {
+        $body.off('click', '.btn-confirm').on('click', '.btn-confirm', function(){
+            var billType = $('#bill-title-select').val(),
+                _billTitle = $('.bill-title-input').val(),
+                billTitle = '';
+            var pattern=/[`~!@#\$%\^\&\*\(\)_\+<>\?:"\{\},\.\\\/;'\[\]]/im;
+            if (billType == 0) {
                 alert('请选择发票类型')
-                //console.log(event)
                 return false;
-            } else {
-                if (billTitle == '') {
-                    alert('请填写发票抬头')
+            }
+            else {
+                if (_billTitle == '') {
+                    alert('请填写发票抬头');
                     return false;
+                }else{
+                    if(pattern.test(_billTitle)){
+                        for (var i = 0; i < _billTitle.length; i++) {
+                            billTitle = billTitle+_billTitle.substr(i, 1).replace(pattern, '');
+                        }
+                        if(billTitle.length > 50){
+                            alert('发票抬头不能超过50个汉字');
+                            return false;
+                        }
+                    }else{
+                        billTitle = _billTitle;
+                        if(billTitle.length > 50){
+                            alert('发票抬头不能超过50个汉字');
+                            return false;
+                        }
+                    }
                 }
             }
-            $(this).css({'cursor': 'pointer'});
+            $(this).attr('disabled',true);
+            $(this).addClass('btn-confirm-disable');
             $.ajax({
-                url : '/MyOrders/ajaxInvoiceAdd',
+                url : 'http://myapache.com/xingzhenli/Test.php?param=1',
                 type: 'post',
-                dataType: 'json',
+                dataType: 'jsonp',
+                jsonp: 'jsonCallBack',
                 data:{
-                    send_id : addId,
-                    order_nums : arr,
+                    id : id,
                     invoice_type : billType,
-                    invoice_title : billTitle,
-                    invoice_note : billTip
+                    invoice_title : billTitle
                 },
-                success: function(d){
-                    var resDat = d;
-                    if(resDat){
-                        checkInput.parents('.bill-details').remove();
+                success: function(result){
+                    if(result.sign == 1){
+                        $('.bill-mask .btn-confirm').attr("disabled",false);
+                        $('.bill-mask .btn-confirm').removeClass('btn-confirm-disable');
+                        $('.bill-mask').remove();
+                        that.parents('.bill-status').addClass('bill-hidden').next().removeClass('bill-hidden');
+                        that.parents('.bill-status').addClass('bill-hidden').next().find('a').attr('href', result.msg);
                     }
-                    else{
-                        $('.bill-check-more').remove();
-                        var pm='<div class="media" style="height: 270px;text-align:center;line-height:90px;color:#666;">该年级下没有老师</div>';
-                        $('.bill-details-list').append(pm);
+                    else if (result.sign == 0) {
+                        $(this).attr("disabled",false);
+                        $(this).removeClass('btn-confirm-disable');
+                        $('.bill-mask .bill-tips-error').text(result.msg);
                     }
-                    window.location.href = '/MyOrders/invoice';
                 }
             });
-        }
-    })
+        });
+    });
+    
+    // var $bsb = $('.bill-sum-button');
+    // //提交按钮
+    // $bsb.on('click',function(event){
+    //     var checkInput = $body.find("input[type='checkbox']:checked");
+    //     var addId = $('input[type="radio"]:checked').attr('id').match(/\d+$/)[0],
+    //         billType = $('#bill-title-select').val(),
+    //         billTitle = $('.bill-title-input').val(),
+    //         billTip = $('#remarkFocus').val();
+    //     var arr = [],
+    //         textNum;
+    //     checkInput.parents('.bill-details').find('.bill-order-num em').each(function (k,v) {
+    //         textNum = $(this).text();
+    //         arr.push(textNum)
+    //     });
+    //     if(checkInput.length == 0){
+    //         event.preventDefault();
+    //     }else {
+    //         if (billType == '请选择') {
+    //             alert('请选择发票类型')
+    //             //console.log(event)
+    //             return false;
+    //         } else {
+    //             if (billTitle == '') {
+    //                 alert('请填写发票抬头')
+    //                 return false;
+    //             }
+    //         }
+    //         $(this).css({'cursor': 'pointer'});
+    //         $.ajax({
+    //             url : '/MyOrders/ajaxInvoiceAdd',
+    //             type: 'post',
+    //             dataType: 'json',
+    //             data:{
+    //                 send_id : addId,
+    //                 order_nums : arr,
+    //                 invoice_type : billType,
+    //                 invoice_title : billTitle,
+    //                 invoice_note : billTip
+    //             },
+    //             success: function(d){
+    //                 var resDat = d;
+    //                 if(resDat){
+    //                     checkInput.parents('.bill-details').remove();
+    //                 }
+    //                 else{
+    //                     $('.bill-check-more').remove();
+    //                     var pm='<div class="media" style="height: 270px;text-align:center;line-height:90px;color:#666;">该年级下没有老师</div>';
+    //                     $('.bill-details-list').append(pm);
+    //                 }
+    //                 window.location.href = '/MyOrders/invoice';
+    //             }
+    //         });
+    //     }
+    // })
 //
 //});
 
